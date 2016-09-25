fe35b268cf5d9edfb5ed47dc901bb3098720afa0
Stéphane Sol <stephane.sol@gmail.com>
2016-09-24 23:52:29 -0500

    clean up files

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -1,9 +1,15 @@
 from functions import *
 import os
 import re
+import json
+
+##todo https://stackoverflow.com/questions/4576077/python-split-text-on-sentences
+##todo finish running files through cleaner 21 - 39 still needed
+
+page_num = '39'
 
 output = os.listdir('/Users/stephane.sol/Developer/OfficialProjectOCR/OutPut')
-input = '/Users/stephane.sol/Developer/OfficialProjectOCR/Data/Text/39.txt'
+input = '/Users/stephane.sol/Developer/OfficialProjectOCR/Data/Text/{0}.txt'.format(page_num)
 
 page = parse(input)
 
@@ -20,8 +26,8 @@ class word(object):
         self.french_sent = french_sent
         self.french_sent2 = french_sent2
 
-    def __repr__(self):
-        return "<page:%s entry:%s english_word:%s french_word:%s english_sent:%s english_sent2:%s french_sent:%s french_sent2:%s>" % (self.page, self.entry, self.english_word, self.french_word, self.english_sent, self.english_sent2, self.french_sent, self.french_sent2)
+    # def __repr__(self):
+    #     return "page:%s entry:%s english_word:%s french_word:%s english_sent:%s english_sent2:%s french_sent:%s french_sent2:%s" % (self.page, self.entry, self.english_word, self.french_word, self.english_sent, self.english_sent2, self.french_sent, self.french_sent2)
 
 for idx, val in enumerate(page[1::]):
 
@@ -32,30 +38,36 @@ for idx, val in enumerate(page[1::]):
     ##split2 = re.sub(r'.([A-Z][^.!?]*(?:[.!?](?![\'"]?\s|$)[^.!?]*)*[.!?]?[\'"]?(?=\s|$))',r'|\1', split[0]).split('|')
     split2 = re.sub(r'([a-z]{1,2}\.(?!\s[a-z]{1,2}\.)|\(.*\)(?!\s[a-z]{1,2}\.)|\s(?![a-z]{1,2}\.)(?!\(.*\))(?!m\s)(?!pl\s)(?!f\s))',r'\1|', split[0],1).split('|')
 
+# print(split)
+# print(split2)
 
 
     try:
-        entry = idx + 1
+        entry = idx
         page_num = page[0]
         entry_key = ('{0}-{1}').format(page_num, entry)
 
-        english_sentence = split[2]
-        french_sentence = split[1]
-        english_word = split2[1]
-        french_word = split2[0]
+        english_sentence = split[2].strip()
+        french_sentence = split[1].strip()
+        english_word = split2[1].strip()
+        french_word = split2[0].strip()
         try:
-            french_sent2 = split[3]
+            french_sent2 = split[3].strip()
         except IndexError:
             french_sent2 = ''
         try:
-            english_sent2 = split[4]
+            english_sent2 = split[4].strip()
         except IndexError:
             english_sent2 = ''
     except IndexError:
-            print('Error at {0} - {1}'.format(entry_key,french_word))
+            print('Error at {0} - {1}'.format(entry_key, split))
 
     dict[entry_key] = word(page=page_num, entry=entry, english_word=english_word,
                            french_word=french_word, english_sent=english_sentence,
                            english_sent2=english_sent2, french_sent=french_sentence, french_sent2=french_sent2)
-
-print(dict)
+#
+# def jdefault(o):
+#     return o.__dict__
+#
+# key = '{0}-{1}'.format(page_num,15)
+# print(json.dumps(dict[key], indent=4, ensure_ascii=False, default=jdefault))
